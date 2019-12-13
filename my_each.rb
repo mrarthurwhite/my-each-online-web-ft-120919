ff035702bdff862e0fe81f2612a9b6fffddac676
Arthur White <mister.arthur.white@gmail.com>
2019-12-13 04:26:03 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+# iterate through the array
+# "yield" each element to a block of code
+# do something in the block
+# after we iterate, we should return the original array
+
+  index = 0
+  while index < array.length
+    yield ( array[index] )
+    index +=1
+  end
+  return array
 end
+
+my_each( [1,2,3]) do |name|
+
+end
+
